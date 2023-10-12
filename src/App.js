bc6e9b2f813e1a0f06d7b4395b5e6f15ee11badc
Lauri Itkonen <lauri.itkonen@hotmail.fi>
2023-10-12 19:17:47 +0300

    Valmis

--- src/App.js
+++ src/App.js
@@ -5,9 +5,10 @@ function App() {
   const [randomDogImage, setRandomDogImage] = useState('');
   const [searchBreed, setSearchBreed] = useState('');
   const [breedImages, setBreedImages] = useState([]);
+  const [error, setError] = useState('');
 
   useEffect(() => {
-    // Hakee koirakuvan
+    // Hakee satunnaisen koirakuvan
     fetch('https://dog.ceo/api/breeds/image/random')
       .then((response) => response.json())
       .then((data) => setRandomDogImage(data.message));
@@ -15,21 +16,26 @@ function App() {
 
   const handleSearch = () => {
     if (searchBreed.trim() === '') {
+      setError('Syötä koiran rotu.');
+      setBreedImages([]);
       return;
     }
 
-    // Rotu haku
+    // Hae kuvia koirarodusta
     fetch(`https://dog.ceo/api/breed/${searchBreed.toLowerCase()}/images/random/10`)
       .then((response) => response.json())
       .then((data) => {
         if (data.status === 'success') {
+          setError('');
           setBreedImages(data.message);
         } else {
+          setError('Rotua ei löytynyt. Syötä kelvollinen koiran rotu.');
           setBreedImages([]);
         }
       })
       .catch((error) => {
-        console.error('Error fetching breed images:', error);
+        console.error('Virhe koirakuvien hakemisessa:', error);
+        setError('Tapahtui virhe. Yritä uudelleen.');
         setBreedImages([]);
       });
   };
@@ -38,26 +44,27 @@ function App() {
     <div className="App">
       <h1>Satunnainen koirakuva</h1>
       <div>
-        <img src={randomDogImage} alt="Random Dog" />
+        <img src={randomDogImage} alt="Satunnainen koira" />
       </div>
 
       <div>
         <h2>Hae koiranrotu</h2>
         <input
           type="text"
-          placeholder="Enter a dog breed"
+          placeholder="Syötä koiran rotu"
           value={searchBreed}
           onChange={(e) => setSearchBreed(e.target.value)}
         />
-        <button onClick={handleSearch}>Search</button>
+        <button onClick={handleSearch}>Hae</button>
       </div>
 
       <div>
         <h2>Hakutulokset</h2>
+        {error && <p className="error">{error}</p>}
         <div className="dog-images">
           {Array.isArray(breedImages) &&
             breedImages.map((imageUrl, index) => (
-              <img key={index} src={imageUrl} alt={`Dog ${index}`} />
+              <img key={index} src={imageUrl} alt={`Koira ${index}`} />
             ))}
         </div>
       </div>
